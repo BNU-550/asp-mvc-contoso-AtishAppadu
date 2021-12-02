6a3afe27a2d53f52b8ce2b61a5d7ac10224fc2e8
Atish <71987929+AtishAppadu@users.noreply.github.com>
2021-12-02 17:01:48 +0000

    Add project files.

---
+++ Models/Enrollment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP_MVC_Contoso.Models
+{
+
+    public enum Grades
+    {
+        A, B, C, D, F
+    }
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+
+        [DisplayFormat(NullDisplayText = "No Grade")]
+        public Nullable<Grades> Grade { get; set; }
+
+        //navigation properties 
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace ASP_MVC_Contoso.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }  
+        [DisplayName("LastName")]
+        public string LastName { get; set; }
+
+        [DisplayName("FirstName")]
+        public string FirstName { get; set; }
+        public DateTime EnrollmentData { get; set; }
+
+        // Relationship properties 
+
+        public virtual ICollection<Enrollment>Enrollments { get; set; }
+        public string FirstMidName { get; internal set; }
+        public DateTime EnrollmentDate { get; internal set; }
+
+        public string FullName()
+        {
+            return FirstName + " " + LastName; 
+        }
+    }
+}
